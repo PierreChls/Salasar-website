204ee0b78925dfaabc39fe34ff3baaeac65000e4
Pierre Charles <pierre@soixantecircuits.fr>
2016-06-21 22:11:42 +0200

    Fix url
    Former-commit-id: 7ec755f60188d3665f5e3cbe11fa2bac29cc99a2
    Former-commit-id: fdaf45235f552998f69731114d8f93ba316d1098
    Former-commit-id: fc8e33b5519f6a09771efcfeaec9d1fba6965617
    Former-commit-id: 577390a89a3dd7bc7a4d746130e9c5977dd25a16
    Former-commit-id: e699624b45519e86449ccafcbff5746f4bcefe98

--- wine-grower/js/main.js
+++ wine-grower/js/main.js
@@ -1,4 +1,6 @@
-$("div.main-container.nobg.container-video").append("<video muted preload='auto' autoplay='true' loop><source src='http://WINE-GROWER.COM/wp-content/themes/wine-grower/BigVideo/vids/wine-grower.mp4' type='video/mp4' /><source src='http://WINE-GROWER.COM/wp-content/themes/wine-grower/BigVideo/vids/wine-grower.webm' type='video/webm' /><source src='http://WINE-GROWER.COM/wp-content/themes/wine-grower/BigVideo/vids/wine-grower.ogv' type='video/ogg' /></video>");
+var url = window.location.href;
+
+$("div.main-container.nobg.container-video").append("<video controls preload='auto' autoplay='true' loop><source src='" + url + "/wp-content/themes/wine-grower/BigVideo/vids/wine-grower.mp4' type='video/mp4' /><source src='" + url + "/wp-content/themes/wine-grower/BigVideo/vids/wine-grower.webm' type='video/webm' /><source src='" + url + "/wp-content/themes/wine-grower/BigVideo/vids/wine-grower.ogv' type='video/ogg' /></video>");
 
 
 function MM_validateForm() { //v4.0
