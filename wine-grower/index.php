62e666a88bea3eba197f14dc33312f0cab466ab1
Pierre Charles <pierre@soixantecircuits.fr>
2016-07-31 20:30:08 +0200

    Update with default API key
    Former-commit-id: a6f2417befd422c88043f500c7ca7f6575ef3d3c
    Former-commit-id: ecabd4afa1c743cbe4f5936de25fcbf4596ec095
    Former-commit-id: 2c75504f3b996f7572915bd10576d5a4ce2c580a
    Former-commit-id: 7add4a334a377d51f91aff8841a20211b5000711
    Former-commit-id: 38b3b22a64597cc3ea7b1dc158bfc80e783beb83

--- wine-grower/index.php
+++ wine-grower/index.php
@@ -123,7 +123,7 @@
          
          <div id="map-canvas">
          
-         <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+         <script type="text/javascript" src="http://maps.google.com/maps/api/js?HERE-YOUR-API-KEY&sensor=true"></script>
          <script src="<?php bloginfo('template_directory'); ?>/js/home-script.js"></script>
 		</div>
          
